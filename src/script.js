641b4813ae6190d62d4269f948dcd835afa9b9f3
Brij Kishor <80624255+TCABrij@users.noreply.github.com>
2023-04-14 17:30:56 +0530

    adding html manually

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const extensions = [
+  {
+    title: "VSCode Extensions",
+    extensions: [
+      "Prettier - Code Formatter",
+      "Eslint",
+      "Code Spell Checker",
+      " Auto Rename Tag",
+      "ES7 + React/Redux",
+      "Material Icon Themes",
+      "Quokka.js - Js & TS Playground",
+      "EJS Language Support",
+    ],
+  },
+  {
+    title: "Chrome Extensions",
+    extensions: [
+      "Prettier - Code Formatter",
+      "Eslint",
+      "Code Spell Checker",
+      " Auto Rename Tag",
+      "ES7 + React/Redux",
+      "Material Icon Themes",
+      "Quokka.js - Js & TS Playground",
+      "EJS Language Support",
+    ],
+  },
+];
