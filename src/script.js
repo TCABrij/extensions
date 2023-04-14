2fc65f1b1824077cfe40cccdf754e67375b47b64
TCABrij <exreply7878@gmail.com>
2023-04-14 21:44:56 +0530

    chrome extensions

--- src/script.js
+++ src/script.js
@@ -5,24 +5,61 @@ const extensions = [
       "Prettier - Code Formatter",
       "Eslint",
       "Code Spell Checker",
-      " Auto Rename Tag",
+      "Auto Rename Tag",
       "ES7 + React/Redux",
       "Material Icon Themes",
       "Quokka.js - Js & TS Playground",
       "EJS Language Support",
+      "Language Specific Ext.",
     ],
   },
   {
     title: "Chrome Extensions",
     extensions: [
-      "Prettier - Code Formatter",
-      "Eslint",
-      "Code Spell Checker",
-      " Auto Rename Tag",
-      "ES7 + React/Redux",
-      "Material Icon Themes",
-      "Quokka.js - Js & TS Playground",
-      "EJS Language Support",
+      "Adguard Adblocker",
+      "AdBlock for YouTube",
+      "Pesticide: Outline for All ",
+      "Grammarly",
+      "JSON Formatter",
+      "React Developer Tools",
+      "Momentum - New Tab",
+      "Volume Booster",
+      "Enhancer for YouTube",
+      "HTML Tree Generator",
+      "Video Speed Controller",
+      "Window Resizer",
     ],
   },
 ];
+
+// Get the container element to hold the cards
+const container = document.querySelector("main");
+
+// Loop through each object in the extensions array
+extensions.forEach((item) => {
+  // Create a new card element
+  const card = document.createElement("div");
+  card.className = "card";
+
+  // Add the card title
+  const title = document.createElement("h2");
+  title.className = "extension-title";
+  title.textContent = item.title;
+  card.appendChild(title);
+
+  // Add the list of extensions
+  const list = document.createElement("ul");
+  list.className = "list";
+
+  item.extensions.forEach((extension) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-items";
+    listItem.textContent = extension;
+    list.appendChild(listItem);
+  });
+
+  card.appendChild(list);
+
+  // Add the card to the container
+  container.appendChild(card);
+});
